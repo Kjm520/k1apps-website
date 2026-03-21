238a792c2c7d3ccb07047e8173f0d2cc586ad3df
Kevin <kjm520@gmail.com>
2026-03-21 14:22:08 -0500

    fix paths

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("assets");
 	eleventyConfig.addPassthroughCopy("css");
 	eleventyConfig.addPassthroughCopy("CNAME");
+	eleventyConfig.addPassthroughCopy("apps/truckload-io/imgs");
 
 	return {
 		dir: {
